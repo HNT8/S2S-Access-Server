c6293d8b7ba4b076be01e9c1ad97f53cf7bc30e4
Hunter <hunterpollock2006@gmail.com>
2022-03-23 22:36:54 -0400

    RunFolder

---
+++ Remote Access/Commands/RunFolder.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using System.IO;
+
+namespace S2S.Commands
+{
+    public class RunFolder : Command
+    {
+        public string CommandName => "run";
+
+        public string CommandSubName => "folder";
+
+        public void Execute(string command)
+        {
+            try
+            {
+                if (command == "documents")
+                {
+                    ProcessStartInfo startInfo = new ProcessStartInfo
+                    {
+                        Arguments = command,
+                        FileName = "explorer.exe"
+                    };
+                    Process.Start(startInfo);
+                    return;
+                }
+
+                ProcessStartInfo explorer = new ProcessStartInfo
+                {
+                    Arguments = command,
+                    FileName = "explorer.exe"
+                };
+                Process.Start(explorer);
+                return;
+            }
+            catch { }
+        }
+    }
+}
--- Remote Access/Program.cs
+++ Remote Access/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
@@ -11,8 +8,6 @@ namespace S2S
 {
     internal class Program
     {
-
-
         static void Main(string[] args)
         {
             Console.Title = "S2S Remote Access Server";
